f8f4eb16d2459a52f889400a55f435090fc2fb97
Rauf Dimaampao <adimaampao@stratpoint.com>
2022-02-25 13:30:04 +0800

    fixed payment amount calculations

--- controller/bookings/getPaymentAmount.js
+++ controller/bookings/getPaymentAmount.js
@@ -2,11 +2,11 @@ const Bookings = require("../../models/bookings/bookings");
 
 module.exports = async (id) => {
   const booking = await Bookings.findById(id);
+  let payment_amount = 0;
 
-  const payment_amount =
-    +booking?.payment?.length === 0
-      ? 0
-      : booking?.payment?.map((e) => e.payment_amount)[0];
+  if (booking?.payment?.length !== 0) {
+    booking?.payment?.map((e) => (payment_amount += e.payment_amount));
+  }
 
   return payment_amount;
 };
