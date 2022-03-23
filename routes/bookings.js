97663272b80ae7690644046bde954bc26d375ac8
Rauf Dimaampao <abdulraufdimaampao@ADimaampao-EDP-100-365.local>
2022-03-23 23:41:59 +0800

    added route for walk in booking

--- routes/bookings.js
+++ routes/bookings.js
@@ -23,6 +23,10 @@ const moment = require("moment-timezone");
 const addDiscount = require("./../controller/bookings/addDiscount");
 const addAmenity = require("./../controller/bookings/addAmenity");
 const auth = require("./../middleware/auth");
+const {
+  createOnlineBooking,
+  createWalkinBooking,
+} = require("./../controller/bookings/createBooking");
 
 // Get Bookings
 router.post("/bookings", async (req, res) => {
@@ -70,67 +74,9 @@ router.post("/booking", async (req, res) => {
   }
 });
 
-router.post("/create_booking", async (req, res) => {
-  const { body } = req;
+router.post("/create_booking", createOnlineBooking);
 
-  const booking_reference = createBookingReference();
-  let date_time_today = moment.tz("Asia/Manila");
-  // Create Billing
-
-  const createNewBilling = () => {
-    const { rooms } = body;
-    let total_amount = 0;
-    rooms.forEach((room) => {
-      total_amount += room.room_amount;
-    });
-
-    return total_amount;
-  };
-
-  const createExpirationDate = () => {
-    const check_in_format = moment(body.check_in).format("YYYY-MM-DD");
-    const tommorow_check_in =
-      moment.tz("Asia/Manila").add(1, "days").format("YYYY-MM-DD") ===
-      check_in_format;
-
-    if (tommorow_check_in) {
-      return moment.tz("Asia/Manila").endOf("day").format();
-    } else {
-      return moment.tz("Asia/Manila").add(1, "days").format();
-    }
-  };
-
-  const newBookings = new Bookings({
-    ...body,
-    booking_reference,
-    expiration_date: createExpirationDate(),
-    status: bookingStatus.PENDING,
-    booking_type: bookingType.ONLINE,
-    billing: {
-      discount: {
-        type: "",
-        amount: 0,
-      },
-      sub_total: createNewBilling(),
-      total_amount: body.totalAmount,
-      additional_total: 0,
-    },
-    events: [createEvent(eventType.BOOKING_CREATED)],
-    payment: [],
-    additionals: [],
-    createdAt: date_time_today.format(),
-    updatedAt: date_time_today.format(),
-  });
-
-  try {
-    let result = await newBookings.save();
-    sendEmail(result, { type: bookingStatus.PENDING });
-    res.status(200).send(newBookings);
-  } catch (error) {
-    console.log(error);
-    res.status(400).send(error);
-  }
-});
+router.post("/create_booking_walkin", createWalkinBooking);
 
 router.post("/update_booking_status", auth, async (req, res) => {
   const { body, user } = req;
